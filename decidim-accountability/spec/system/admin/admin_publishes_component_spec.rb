c424fb266546d60f6d9c820e195a435b86f3b83a
Alexandru Emil Lupu <contact@alecslupu.ro>
2026-02-18 15:28:58 +0200

    Add clear_enqueued_jobs on accountability

--- decidim-accountability/spec/system/admin/admin_publishes_component_spec.rb
+++ decidim-accountability/spec/system/admin/admin_publishes_component_spec.rb
@@ -6,6 +6,10 @@ describe "Admin publishes component" do
   let(:manifest_name) { "accountability" }
   let!(:resource) { create(:result, component:) }
 
+  before do
+    clear_enqueued_jobs
+  end
+
   include_context "when publishing and unpublishing the component"
   include_context "when cycling through publication states"
 end
